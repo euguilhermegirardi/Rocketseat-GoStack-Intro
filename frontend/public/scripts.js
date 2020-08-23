4cf7f672bb31fc721e656229c2308c046781d737
Guilherme Girardi <girardi.gui@icloud.com>
2020-08-23 06:23:49 +0100

    Create project. Config with babel and webpack.

---
+++ frontend/public/scripts.js
@@ -0,0 +1,6 @@
+/**
+ * Babel: Convert the React code into a code that the browser understands.
+ * Webpack: For each one of the files (.js, .css, .png) it will convert it into a different way.
+ *
+ * Loaders: babel-loader, css-loader, image-loader
+ */
