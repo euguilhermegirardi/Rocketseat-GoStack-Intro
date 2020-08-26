dd35e3d41745bc720a66edf43e1af1b3ce890a5c
Guilherme Girardi <girardi.gui@icloud.com>
2020-08-26 20:20:17 +0100

    Create repositories list

---
+++ mobile/src/index.js
@@ -0,0 +1,47 @@
+import React, {useState, useEffect} from 'react';
+import {
+  SafeAreaView,
+  FlatList,
+  Text,
+  StyleSheet,
+  StatusBar,
+} from 'react-native';
+
+import api from './services/api';
+
+export default function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then((res) => {
+      console.log(res.data);
+      setProjects(res.data);
+    });
+  }, []);
+
+  return (
+    <>
+      <StatusBar barStyle="light-content" />
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={projects}
+          keyExtractor={(project) => project.id}
+          renderItem={({item: project}) => (
+            <Text style={styles.project}>{project.title}</Text>
+          )}
+        />
+      </SafeAreaView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+  },
+  project: {
+    color: '#FFF',
+    fontSize: 20,
+  },
+});
